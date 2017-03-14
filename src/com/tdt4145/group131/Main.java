6e47b5256779d19255e41731c9dec4a2743bb218
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-14 19:36:57 +0100

    Removed all traces of fxml

--- src/com/tdt4145/group131/Main.java
+++ src/com/tdt4145/group131/Main.java
@@ -3,24 +3,10 @@ package com.tdt4145.group131;
 import com.tdt4145.group131.database.models.ExerciseGroup;
 import com.tdt4145.group131.database.DatabaseService;
 import com.tdt4145.group131.database.models.Session;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.*;
 
 import java.sql.Timestamp;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/com/tdt4145/group131/application.fxml"));
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setTitle("Training Diary");
-        stage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
         try{
@@ -51,7 +37,6 @@ public class Main extends Application {
 
             dbs.getAllExerciseGroups();
             dbs.getExerciseGroupById(2);
-            // launch(args);
         }
         catch (Exception e) {
             e.printStackTrace();
