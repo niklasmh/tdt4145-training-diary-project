8442bda5800abb4889638c33e2dcb41d6800538a
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-14 20:36:00 +0100

    Added the view handler to main

--- src/com/tdt4145/group131/Main.java
+++ src/com/tdt4145/group131/Main.java
@@ -1,10 +1,13 @@
 package com.tdt4145.group131;
 
+import com.tdt4145.group131.view.ViewHandler;
+
 public class Main {
 
     public static void main(String[] args) {
         try {
-            
+            ViewHandler vh = new ViewHandler();
+            vh.listExerciseGroups();
         } catch (Exception e) {
             e.printStackTrace();
         }
