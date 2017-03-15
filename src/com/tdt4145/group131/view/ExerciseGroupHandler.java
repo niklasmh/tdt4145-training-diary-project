f2c49f1b9afd287043f1b77bbafd2a6cd5afeeaf
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-03-15 01:52:43 +0100

    Added update handler for exercise groups

--- src/com/tdt4145/group131/view/ExerciseGroupHandler.java
+++ src/com/tdt4145/group131/view/ExerciseGroupHandler.java
@@ -21,6 +21,8 @@ public class ExerciseGroupHandler {
 
     public void runHandler () {
 
+        String name;
+
         while (true) {
             int menu = this.getMenuSelect();
 
@@ -31,14 +33,14 @@ public class ExerciseGroupHandler {
                     ExerciseGroupHandler.listExerciseGroups();
                     break;
                 case 2:
-                    String name = vh.getStringFromQuestion(
+                    name = vh.getStringFromQuestion(
                             "The exercise group name: ",
                             "[A-Za-zøæåØÆÅ ]+",
                             "Wrong format. Type group name again: "
                     );
 
                     String addPartOf = vh.getStringFromQuestion(
-                            "Want to add this exercise group as part of another?\n[Y]es or [n]o (default if empty): ",
+                            "Want to add this exercise group as part of another?\n[y]es or [N]o (no if empty): ",
                             "^[YyNn]?$",
                             "Wrong format. Type [y]es or [N]o again: "
                     );
@@ -55,9 +57,81 @@ public class ExerciseGroupHandler {
                     } else {
                         this.addExerciseGroup(name, 0);
                     }
+
                     break;
                 case 3:
-                    System.out.println("Handle 3");
+                    System.out.println("\n\nHere are all exercises you can update:");
+                    ExerciseGroupHandler.listExerciseGroupsIndexed();
+
+                    int id = vh.getIntFromQuestion(
+                            "Which do you want to update? ",
+                            "[0-9]+",
+                            "Wrong format. Type id again: "
+                    );
+
+                    try {
+                        ExerciseGroupService es = new ExerciseGroupService();
+                        ExerciseGroup oldExerciseGroup = es.getExerciseGroupById(id);
+
+                        name = vh.getStringFromQuestion(
+                                "New name: (Previous was: " + oldExerciseGroup.name + ", leave blank to remain name unchanged)",
+                                "[A-Za-zøæåØÆÅ ]*",
+                                "Wrong format. Type group name again: "
+                        );
+
+                        int parentGroupId = 0;
+
+                        if (oldExerciseGroup.parent_group_id > 0) {
+                            ExerciseGroup oldParentGroup = es.getExerciseGroupById(oldExerciseGroup.parent_group_id);
+
+                            String updatePartOf = vh.getStringFromQuestion(
+                                    "The current group has a parent ([" +
+                                            oldExerciseGroup.parent_group_id +
+                                            "] " +
+                                            oldParentGroup.name +
+                                            "). Want to change it?\n[y]es or [N]o (no if empty): ",
+                                    "^[YyNn]?$",
+                                    "Wrong format. Type [y]es or [N]o again: "
+                            );
+
+                            if (updatePartOf.matches("^[Yy]$")) {
+                                System.out.println("Here is a list of available parents:");
+                                ExerciseGroupHandler.listExerciseGroupsIndexed();
+
+                                parentGroupId = vh.getIntFromQuestion(
+                                        "New parent id: (Previous was: " +
+                                                oldExerciseGroup.parent_group_id +
+                                                " (" +
+                                                oldParentGroup.name +
+                                                "), leave blank to remain id unchanged)",
+                                        "[0-9]*",
+                                        "Wrong format. Type parent id again: "
+                                );
+                            }
+                        } else {
+                            String updatePartOf = vh.getStringFromQuestion(
+                                    "The current group does not have a parent. Want to add a parent?\n[y]es or [N]o (no if empty): ",
+                                    "^[YyNn]?$",
+                                    "Wrong format. Type [y]es or [N]o again: "
+                            );
+
+                            if (updatePartOf.matches("^[Yy]$")) {
+                                System.out.println("Here is a list of available parents:");
+                                ExerciseGroupHandler.listExerciseGroupsIndexed();
+
+                                parentGroupId = vh.getIntFromQuestion(
+                                        "New parent id: (Leave blank to remain id empty)",
+                                        "[0-9]*",
+                                        "Wrong format. Type parent id again: "
+                                );
+                            }
+                        }
+
+                        this.updateExerciseGroup(oldExerciseGroup, name, parentGroupId);
+                    } catch (SQLException ex) {
+                        System.out.println("Could not get previous exercise group. Try again later or change another one.");
+                    }
+
                     break;
             }
         }
@@ -70,9 +144,10 @@ public class ExerciseGroupHandler {
                         "\n[0] Go back" +
                         "\n[1] List all exercise groups" +
                         "\n[2] Add exercise group" +
+                        "\n[3] Update exercise group" +
                         "\n\nType number: ",
-                "^[0-2]$",
-                "Please provide a number between 0 and 2: "
+                "^[0-3]$",
+                "Please provide a number between 0 and 3: "
         );
     }
 
@@ -86,6 +161,26 @@ public class ExerciseGroupHandler {
         try {
             es.saveNewExerciseGroup(exerciseGroup);
             System.out.println("\nAdded exercise group to database!");
+        } catch (SQLException ex) {
+            System.out.println("Could not save exercise group. Try again later.");
+        } catch (Exception ex) {
+            System.out.println("Error in code. Please ask admin.");
+        }
+    }
+
+    public void updateExerciseGroup (ExerciseGroup oldExerciseGroup, String name, int parentGroupId) {
+        ExerciseGroupService es = new ExerciseGroupService();
+
+        ExerciseGroup exerciseGroup = new ExerciseGroup();
+        exerciseGroup.name = name.length() > 0 ? name : oldExerciseGroup.name;
+        exerciseGroup.parent_group_id = parentGroupId > 0 ? parentGroupId : oldExerciseGroup.parent_group_id;
+
+        try {
+            if (es.updateExerciseGroup(exerciseGroup)) {
+                System.out.println("\nUpdated exercise group in database!");
+            } else {
+                System.out.println("\nCould not update exercise group in database.");
+            }
         } catch (SQLException ex) {
             System.out.println("Could not save exercise group. Try again later.");
         } catch (Exception ex) {
